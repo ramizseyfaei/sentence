92d6d6f51009c3f9bed048543b8ec5de0e3beaf4
ramizseyfaei <ramizseyfaei@gmail.com>
2023-03-15 23:54:27 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Fragment, useEffect } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import {
   sentenceadminadd,
   categoryall,
