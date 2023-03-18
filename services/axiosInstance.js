bb6bd7d1bfa8c6e0d8df7588810c60f0f6abad78
ramizseyfaei <ramizseyfaei@gmail.com>
2023-03-18 12:05:06 +0330

    zsdf

--- services/axiosInstance.js
+++ services/axiosInstance.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://api.prorobo.ir",
+  withCredentials: true,
 });
 
 // axiosInstance.interceptors.request.use((config) => {
