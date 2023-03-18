273cc390457aaea5a8b4a4322137c633497fc341
ramizseyfaei <ramizseyfaei@gmail.com>
2023-03-18 12:09:51 +0330

    Update axiosInstance.js

--- services/axiosInstance.js
+++ services/axiosInstance.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://api.prorobo.ir",
-  withCredentials: true,
 });
 
 // axiosInstance.interceptors.request.use((config) => {
